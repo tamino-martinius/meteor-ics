b123370c222fbf491188537456007b68b9c74989
Alexis Urvoy <alexis@alexisurvoy.com>
2015-11-04 22:33:21 +0000

    Renaming files

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
   summary: 'iCalendar .ics export',
   version: '1.0.0',
-  git: 'https://github.com/Zaku-eu/meteor-ics.git'
+  git: 'https://github.com/mikotodesign/meteor-ics.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.0');
-  api.addFiles('zaku:ics.js', 'server');
+  api.addFiles('mikotodesign:ics.js', 'server');
   api.export('iCalendar');
 });
